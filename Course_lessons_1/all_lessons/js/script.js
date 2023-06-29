65404d6215bbea9a07b5c8365f808d692c2c6990
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-06-29 11:11:41 +0300

    added third lesson

---
+++ Course_lessons_1/all_lessons/js/script.js
@@ -0,0 +1,38 @@
+/* console.log(1); */
+
+"use strict";
+
+a = 15;
+console.log(a)
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj)
+
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+console.log(result);
+
+const widthCar = 4000;
+const lenghtCar = 5000;
+console.log("Ширина кузова автомобиля: " + widthCar + ", длина: " + lenghtCar);
+
+
+['fhdfhdfhd', 'awdqwqw', 'dasdasd'].map(a => );
+
+//Request
+data
+respose
