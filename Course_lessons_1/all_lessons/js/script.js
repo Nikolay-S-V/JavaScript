16c72213c941659506cdb5e40935a96868feff19
Nikolay <savenko.nikolay.valerevich@gmail.com>
2023-06-29 12:11:33 +0300

    added lesson data types

--- Course_lessons_1/all_lessons/js/script.js
+++ Course_lessons_1/all_lessons/js/script.js
@@ -5,7 +5,7 @@
 a = 15;
 console.log(a)
 
-let number = 5;
+/* let number = 5; */
 const leftBorderWidth = 1;
 
 number = 10;
@@ -36,3 +36,51 @@ console.log("Ширина кузова автомобиля: " + widthCar + ", �
 //Request
 data
 respose
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Nick";
+
+const bool = false;
+
+console.log(somethind);
+
+let und;
+console.log(und)
+
+const obj = {
+    name: 'Nick',
+    age: 25,
+    isMarried: false
+};
+
+/* console.log(obj.name); */
+console.log(obj['name']);
+
+
+let arr = ['plumb.png', 'orange.jpeg', 6, 'apple.bmp', {}, []];
+
+console.log(arr[1]);
+
+const arrrr = ['a', 'b', 'c'];
+const arfs = {
+    a: 'a',
+    1: 'b',
+    2: 'c'
+};
+
+arfs.b = '12345';
+
+/* console.log(arrrr[1]); */
+console.log(arfs.b);
+
+/* const obect = {a: 1, b: 2}; */
+
+const obect = {
+    'anna': 500,
+    'elis': 800
+};
+
